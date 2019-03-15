bd5f987e93a3ef5a5cbedc9384c6bc5008a6dc14
Jorge Cano <jorgeucano@gmail.com>
2019-03-15 19:42:28 -0300

    agrego idea de menu

--- website/src/app/app.module.ts
+++ website/src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import {Routes} from '@angular/router';
+import {MaterialModule} from './material/material.module';
+import { MenuComponent } from './components/menu/menu.component';
+
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home' },
+  { path: 'home', }
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
